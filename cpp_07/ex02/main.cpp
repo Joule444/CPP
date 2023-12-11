47cab6c452d595664c52a76bbad2052607f4adf4
Jules THUYSBAERT <jthuysba@e1r2p15.clusters.42paris.fr>
2023-12-11 16:02:23 +0100

    cpp_07/ex02 : Clean Tests, All Good

--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:07:22 by jthuysba          #+#    #+#             */
-/*   Updated: 2023/12/11 15:55:23 by jthuysba         ###   ########.fr       */
+/*   Updated: 2023/12/11 16:01:15 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main( void )
 {
 	{
-		std::cout << BOLD_YELLOW << "TESTS AVEC INT" << RESET << std::endl;
+		std::cout << BOLD_YELLOW << "TEST AVEC INT" << RESET << std::endl;
 		
 		size_t	len = 10;
 		Array<int>	arr(len);
@@ -25,9 +25,11 @@ int	main( void )
 			std::cout << "[" << CYAN << arr[i] << RESET << "]" << std::endl;
 		}
 	}
+	
 	std::cout << std::endl;
+	
 	{
-		std::cout << BOLD_YELLOW << "TESTS AVEC CHAR" << RESET << std::endl;
+		std::cout << BOLD_YELLOW << "TEST AVEC CHAR" << RESET << std::endl;
 		
 		size_t	len = 10;
 		Array<char>	arr(len);
@@ -35,6 +37,24 @@ int	main( void )
 		for (size_t i = 0; i < len; i++)
 		{
 			std::cout << "[" << CYAN << arr[i] << RESET << "]" << std::endl;
+		}
+	}
+	
+	std::cout << std::endl;
+	
+	{
+		std::cout << BOLD_YELLOW << "TEST EXCEPTION" << RESET << std::endl;
+		
+		Array<char>	arr;
+
+		try
+		{
+			std::cout << arr[5] << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << DARK_RED << "Error: Requested index is out of bounds"
+				<< RESET << '\n';
 		}
 	}
 	
